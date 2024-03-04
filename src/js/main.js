b5ce275859aa961b263deab7e18c1d3220721ed3
Eeemlo <emma.lorensson@outlook.com>
2024-03-04 14:12:44 +0100

    Hela annonser inladdade

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,79 @@
-
-let jobListings = document.getElementById("job-listings");
-
+const jobListings = document.getElementById("job-listings");
 
 //Hämta data för jobbannonser och skriv ut till DOM
 async function loadJobs() {
-    const response = await fetch(`https://links.api.jobtechdev.se/joblinks?occupation-field=apaJ_2ja_LuF&q=frontend?q=backend?q=web?q=Stockholm?q=javascript?q=typescript%20-senior%20-sr%20-AWS%20-golang&limit=100`);
-    const jobPosts = await response.json();
-    console.log(jobPosts)//Ta bort
+  const response = await fetch(
+    `https://jobsearch.api.jobtechdev.se/search?occupation-field=apaJ_2ja_LuF&q=Sverige?q=backend?q=web?q=javascript?q=typescript?%20-javautvecklare%20-expert%20-c++%20-manager%20-salesforce%20-ios%20-servicenow%20-architect%20-analyst%20-AI%20-python%20-testledare%20-.net%20-engineer%20-senior%20-sr%20-CD%20-AWS%20-golang%20-bsc%20-msc%20-kandidat%20-master&limit=100`
+  );
+  const jobPosts = await response.json();
+  console.log(jobPosts); //Ta bort
 
-    for (let i = 0; i <= jobPosts.total.value; i++) {
-    jobListings.innerHTML += `
-    <section class="job_card">
-    <h2>${jobPosts.hits[i].headline}</h2>
-        <h3>${jobPosts.hits[i].employer.name}</h3>
-        <h5>${jobPosts.hits[i].workplace_addresses[0].municipality}</h5>
-        <p>${jobPosts.hits[i].brief}</p>
-        <a class="ad_button button" href="${jobPosts.hits[i].source_links[0].url}">Till annonsen</a>
-        </section>`;
-    }
+  console.log(jobPosts.hits[0].headline); //Ta bort
+
+  iterateJobs(jobPosts);
 }
 
+//Funktion för att loopa igenom resultat och skriva ut till DOM
+function iterateJobs(jobPosts) {
+  for (let i = 0; i <= jobPosts.total.value; i++) {
+    //Lägg till += för innerHTML om det  behövs
+    jobListings.innerHTML += ` 
+  <div class="job_card">
+  <div>
+  <h2>${jobPosts.hits[i].headline}</h2>
+      <h3>${jobPosts.hits[i].employer.name}</h3>
+      <h5>${jobPosts.hits[i].workplace_address.municipality}</h5>
+      <p>${jobPosts.hits[i].description.text_formatted}</p>
+      <a class="ad_button button" href="${jobPosts.hits[i].webpage_url}">Till annonsen</a>
+      </div>
+      </div>`;
+  }
+}
 
-    loadJobs()
+ //Läs in jobb
+loadJobs();
 
+//Hämta användarens position
+function getUserLocation(jobPosts) {
+const successCallback = (position) => {
+  console.log(position);
+};
+const errorCallback = (error) => {
+  console.error(error);
+};
 
+navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
+//Funktion för att räkna ut avstånd mellan koordinator för jobb och user location
+
+}
+getUserLocation();
+
+  
+//Variabler för HTML-element
+const searchBar = document.getElementById("searchbar");
+const searchButton = document.getElementById("searchbutton");
+
+//Event vid adressinmatning
+searchButton.onclick = async function () {
+  const errorMsg = document.getElementById("error-msg");
+  let input = searchBar.value;
+
+  jobListings.innerHTML = "";
+  errorMsg.innerHTML = "";
+
+  //Gör fetchanrop
+  const response = await fetch(
+    `https://jobsearch.api.jobtechdev.se/search?occupation-field=apaJ_2ja_LuF&q=${input}?q=backend?q=web?q=javascript?q=typescript?%20-javautvecklare%20-expert%20-c++%20-manager%20-salesforce%20-ios%20-servicenow%20-architect%20-analyst%20-AI%20-python%20-testledare%20-.net%20-engineer%20-senior%20-sr%20-CD%20-AWS%20-golang%20-bsc%20-msc%20-kandidat%20-master&limit=100`
+  );
+  //Lagra svar i variabel
+  const jobPosts = await response.json();
+
+  //Loopa igenom nya resultat efter sökning och skriv ut till DOM
+  if (jobPosts && jobPosts.total.value > 0) {
+    iterateJobs(jobPosts);
+  } else {
+    errorMsg.innerHTML = "Hittade inga annonser";
+  }
+};
 
