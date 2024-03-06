31b79d14792f24d3da9b5a7d6891d78ed43e2285
Eeemlo <emma.lorensson@outlook.com>
2024-03-06 13:36:09 +0100

    Funktion med API för att få ut distans fungerar

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,54 @@
 const jobListings = document.getElementById("job-listings");
-const nextBtn = document.getElementById("nextButton");
-const backBtn = document.getElementById("backButton");
 const yesIcon = document.querySelector(".icon-yes")
 const noIcon = document.querySelector(".icon-no")
 let input = "Sverige";
 let page = 0;
 let skip;
+let i;
+let lat;
+let long;
+
+//Hämta användarens position och returnera long + lat
+function getUserLocation() {
+  const successCallback = (position) => {
+    lat = position.coords.latitude;
+    long = position.coords.longitude;
+    console.log(position);
+    calculateDistance(lat, long);
+  };
+  const errorCallback = (error) => {
+    console.error(error);
+  };
+
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+
+
+  console.log("1");
+
+  //Funktion för att räkna ut avstånd mellan koordinator för jobb och user location
+}
+
+getUserLocation();
+
+//Variabel som används om jag vill använda timer(1000) för att delaya något
+/*const timer = ms => new Promise(res => setTimeout(res, ms));*/
+
+//Hämta data för jobbannonser och skriv ut till DOM
+async function loadJobs() {
+  const response = await fetch(
+    `https://jobsearch.api.jobtechdev.se/search?occupation-field=apaJ_2ja_LuF&q=${input}?q=backend?q=web?q=javascript?q=typescript?%20-javautvecklare%20-expert%20-c++%20-manager%20-salesforce%20-ios%20-servicenow%20-architect%20-analyst%20-AI%20-python%20-testledare%20-.net%20-engineer%20-senior%20-sr%20-CD%20-AWS%20-golang%20-bsc%20-msc%20-kandidat%20-master&limit=1&offset=${page}`
+  );
+  const jobPosts = await response.json();
+
+  console.log(jobPosts)
+
+  await iterateJobs(jobPosts);
+
+  console.log("2");
+}
+
+//Läs in jobb
+loadJobs();
 
 //Variabler för HTML-element
 const searchBar = document.getElementById("searchbar");
@@ -37,22 +80,24 @@ searchButton.onclick = async function () {
 };
 
 
-//Hämta data för jobbannonser och skriv ut till DOM
-async function loadJobs() {
-  const response = await fetch(
-    `https://jobsearch.api.jobtechdev.se/search?occupation-field=apaJ_2ja_LuF&q=${input}?q=backend?q=web?q=javascript?q=typescript?%20-javautvecklare%20-expert%20-c++%20-manager%20-salesforce%20-ios%20-servicenow%20-architect%20-analyst%20-AI%20-python%20-testledare%20-.net%20-engineer%20-senior%20-sr%20-CD%20-AWS%20-golang%20-bsc%20-msc%20-kandidat%20-master&limit=1&offset=${page}`
-  );
-  const jobPosts = await response.json();
 
-  await iterateJobs(jobPosts);
+
+async function calculateDistance(lat, long) {
+  const response = await fetch(
+    `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${lat},${long}&destinations=51.4994794,-0.1269979&key=fA3CPEYhXO1udHHiHApKRfX6jm5Cx1Hk5f9OAxdb7tDitT3dpwdkn0ZUADi9KMST`
+  );
+  const distance = await response.json();
+
+  console.log(distance.rows[0].elements[0].distance.text)
+
+  console.log(lat, long)
+
 }
 
-//Variabel som används om jag vill använda timer(1000) för att delaya något
-const timer = ms => new Promise(res => setTimeout(res, ms));
 
 //Funktion för att loopa igenom resultat och skriva ut till DOM
 async function iterateJobs(jobPosts) {
-  for (let i = 0; i <= jobPosts.total.value; i++) {
+  for (i = 0; i <= jobPosts.total.value; i++) {
     jobListings.innerHTML = ` 
   <div class="job_card">
   <div>
@@ -74,8 +119,17 @@ async function iterateJobs(jobPosts) {
 }
 
 
-//Läs in jobb
-loadJobs();
+
+  //Funktion för att spara annons i localStorage
+/*FUNKAR EJ ÄN!!!
+  function saveToLocalStorage(jobPosts) {
+    localStorage.setItem(`annons${page}`, `${jobPosts.hits[page].id}`);
+    console.log("knappen funkar")
+  
+  }
+  
+  yesIcon.addEventListener("click", saveToLocalStorage, false);
+*/
 
 //Försök till att visa mer eller mindre av texten.. FUNKAR INTE!!!!
 /*
@@ -99,8 +153,9 @@ function playYes() {
       document.querySelector(".job_card").className = "job_card job_card_yes";
     });
   });
-
 }
+
+
 //Trigga funktion "swipeYes"
 yesIcon.addEventListener("click", playYes, false);
 yesIcon.addEventListener("click", increasePageVar, false);
@@ -137,18 +192,6 @@ function truncate(description, maxLength = 800) {
   return truncated + ellipsis;
 }
 
-//Hämta användarens position
-function getUserLocation(jobPosts) {
-  const successCallback = (position) => {
-    console.log(position);
-  };
-  const errorCallback = (error) => {
-    console.error(error);
-  };
 
-  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
 
-  //Funktion för att räkna ut avstånd mellan koordinator för jobb och user location
-}
-getUserLocation();
 
